c6d0dc1541c410958b91dd339665cd3b8b6b016a
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2025-03-02 16:11:11 +0700

    feat: configure Prettier and update global styles; add SVG icons and Tailwind CSS enhancements

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,11 +7,25 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)'],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        lime: "#bbd934",
+        aqua: "#1fbfac",
+        orange: "#ff7518",
+        green: "#16d113",
+        darkgreen: "#0b8909",
+        darkred: "#d12613",
+        darkorange: "#d17b13",
+        darkblue: "#138ed1",
+        lightorange: "#ffd6ba",
+        gray: "#d9d9d9",
+        foreground: "#666666"
       },
     },
   },
   plugins: [],
 };
+
+export default config;
